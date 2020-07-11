4dfcbeeb1bc63d58314b3ce836f3ce35b20e1889
praveena-sadasivam <62805270+praveena-sadasivam@users.noreply.github.com>
2020-07-11 12:40:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,68 +1,75 @@
-from bs4 import BeautifulSoup
 import requests
-import re
-import csv
-import json
+from bs4 import BeautifulSoup
 import spacy
+import json
+import csv
+import re
 
 #load the language model instance in spacy
-nlp =spacy.load('en_core_web_sm')
+nlp =spacy.load('en_core_web_sm') 
 
-#function to get html content of web page using requests module
 def get_webpage(url : str )-> str:
-    try:
-        response = requests.get(url)
-        page_html=response.text
-        return page_html
-    except:
-        return None
+    # Getting the webpage, creating a Response object.
+    response = requests.get(url)
+    # Extracting the source code of the page.
+    page_html = response.text
+    return page_html
 
-#function to get visible text from each page using beautiful soup module
 def get_webpage_text(html : str )-> str:
-    soup = BeautifulSoup(html, "lxml")
-    page_text=soup.text
+    response = requests.get(url)
+ 
+# Extracting the source code of the page.
+    html = response.text
+# Passing the source code to BeautifulSoup to create a BeautifulSoup object for it.
+    soup = BeautifulSoup(html, 'lxml')
+    #Finding the text
+    page_text = soup.text
+    
     return page_text
 
-#function to get company name and url as a list
 def get_list(page_html)->list:
-    soup = BeautifulSoup(page_html, "lxml")
-    content=soup.findAll('a',{'class':'100link'})
-    company_name_url=[]
-    for item in content:
-        if item.text!="View From The Top Profile":
-            company_name_url.append([item.text,item.get('href')])
-    return company_name_url
+    # Passing the source code to BeautifulSoup to create a BeautifulSoup object for it.
+    soup = BeautifulSoup(page_html, 'lxml')
+ 
+# # Extracting all the <a> tags into a list.
+    a_tag = soup.findAll('a', {'class': '100link'})
 
-#
+    company_name_url_list=[]
+    for name in a_tag:
+        if name.text!="View From The Top Profile":
+            company_name_url_list.append([name.text,name.get('href')])
+    return company_name_url_list
+
+
 def get_contact_page_link(html : str )-> list:
-    contact_page_link=[]
-    company_name_url=get_list(html)
-    for item in company_name_url:
-        try:
-            name=item[0]
-            url=item[1]
-            page_html=get_webpage(url)
-            contact_link=[]
-            soup = BeautifulSoup(page_html, "lxml")
-            a_tag=soup.findAll('a')
-            link_only=[]
-            for item in a_tag:
-                link_only.append(item.get('href'))
-            pattern=["contact","about"]
-            for link in link_only:
-                for item in pattern:
-                    if re.search(item,link):
-                        if link.startswith('http'):
-                            contact_link.append(link)
-                        else:
-                            contact_link.append(url+link)
-            contact_page_link.append([name,contact_link])
-            print("got contact of name : ",name)
-        except:
-            print("coudnt get for ",name)
-    return contact_page_link
 
-#
+    # to get list containing companies and url
+    company_name_url_list=get_list(html)
+    contact_list=[]
+    
+    for name_list in company_name_url_list[:1]:
+        try:
+            com_name=name_list[0]
+            com_url=name_list[1]
+            com_html=get_webpage(com_url)
+            soup = BeautifulSoup(com_html, 'lxml')
+            a_tag = soup.findAll('a')
+            patterns=["about"]
+            for item in a_tag:
+                for pattern in patterns:
+                    if re.search(pattern,item.get('href')):
+                        if item.get('href').startswith('http'):
+                            contact_list.append([com_name,item.get('href')])
+                        else:
+                            contact_list.append([com_name,com_url+item.get('href')])
+        except:
+            print("coudnt find contact page for ",com_name)
+    unique_list=[]
+    [unique_list.append(item) for item in contact_list if item not in unique_list ]
+    print(unique_list)
+    return unique_list
+
+
 def get_location(text : str)-> list:
     loc_list=[]
     doc = nlp(text)
@@ -70,56 +77,73 @@ def get_location(text : str)-> list:
     for ent in doc.ents:
             if "GPE" in ent.label_ :
                 loc_list.append(ent.text)
-#remove repeated name and their entity 
+#remove repeated name 
     com_loc_list= [] 
-    [com_loc_list.append(x) for x in loc_list if x not in com_loc_list ]
-
+    [com_loc_list.append(item) for item in loc_list if item not in com_loc_list ]
+    print(com_loc_list)
     return com_loc_list
 
 def save_to_json(filename : str ,json_dict : dict)-> None:
-    with open(filename, "w") as file_object:
-        #write all the entites data in json
-        file_object.write(json.dumps(json_dict, sort_keys=False, indent=2, separators=(',', ': ')))
-    return None
+
+     with open(filename, "w") as file_obj:
+#write all the entites data in json
+            file_obj.write(json.dumps(json_dict))
+     
 
 def json_to_csv_file(json_filename  : str ,csv_filename : str)-> None:
+            # Opening JSON file and loading the data 
+# into the variable data 
     with open(json_filename) as json_file: 
-        data = json.load(json_file)
-    data_file = open(csv_filename, 'w')
-    csv_writer = csv.writer(data_file)
-    
-    for item in data:
-        csv_writer.writerow(item)
-    # create the csv writer object 
-    # csv_writer = csv.writer(data_file) 
+        data =json.load(json_file)
+        temp=[]
+        for i in data:
+            temp.append({"company name":i,"location":data[i]})
+
+        # field names  
+        fields = ["company name","location"]  
+        # writing to csv file  
+        with open(csv_filename, 'w') as csvfile: 
+            # creating a csv dict writer object  
+            writer = csv.DictWriter(csvfile, fieldnames = fields)  
+        
+            # writing headers (field names)  
+            writer.writeheader()  
+        
+            # writing data rows  
+            writer.writerows(temp) 
 
 
-#url
-url="http://www.econtentmag.com/Articles/Editorial/Feature/The-Top-100-Companies-in-the-Digital-Content-Industry-The-2016-2017-EContent-100-114156.htm"
+if __name__ == '__main__':
+    url = "http://www.econtentmag.com/Articles/Editorial/Feature/The-Top-100-Companies-in-the-Digital-Content-Industry-The-2016-2017-EContent-100-114156.htm"
+    html= get_webpage(url)
+    company_list=get_list(html)
+    print(company_list)
+    print("company name and url extracted!!!")
 
-#calling function to get page_html
-page_html=get_webpage(url)
-filename='data.json'
 
-# print(page_html)
+    #to get page html
+    page_html=get_webpage(url)
+    # to get list conataining company name and contact url
+    comName_contactUrl_list=get_contact_page_link(page_html)
 
-# #calling function to get webpage visible text
-# page_text=get_webpage_text(page_html)
-# print(page_text)
-# #calling function to get list containing company name & url
-#company_name_url=get_list(page_html)
-# print(company_name_url)
-#print(get_contact_page_link(page_html))
-# to get list conataining company name and contact url
-comName_contactUrl_list=get_contact_page_link(page_html)
-comp_dict=[]
-for company in comName_contactUrl_list:
-     
-     name=company[0]
-     url=company[1]
-     text=get_webpage_text(url)
-     loction_list=get_location(text)
-     comp_dict[name]=loction_list
-save_to_json(filename,comp_dict)
-json_to_csv_file(json_filename csv_filename)
-    
+    print(" Getting  companyname and contact url list ")
+    print(comName_contactUrl_list)
+    print("\n")
+    all_location=[]
+    filename="details.json"
+    com_dict={}
+    for company in comName_contactUrl_list:
+        name=company[0]
+        url=company[1]
+        print("URL ",url)
+        text=get_webpage_text(url)
+        #  print(text)
+        location_list=get_location(text)
+        all_location+=location_list
+    com_dict[name]=all_location
+    print("\n")
+    print(com_dict)
+    save_to_json(filename,com_dict)
+    print("\nsave to json file successfully\n")
+    json_to_csv_file(filename,"details.csv")
+    print("Json to csv convert sucessfullly\n")
